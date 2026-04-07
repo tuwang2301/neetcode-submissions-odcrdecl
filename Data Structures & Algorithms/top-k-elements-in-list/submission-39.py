4b01f2d004798afd15f67cc938e28a893625abba
Quang Tu Nguyen <quangtu2301@gmail.com>
2026-04-07 18:49:50 +1000

    Add: top-k-elements-in-list - submission-39

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-39.py
@@ -0,0 +1,8 @@
+import heapq
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        c = Counter(nums)
+
+        return sorted(c, key= lambda x : c[x], reverse=True)[:k]
