634c8706e3c71c3f37a220b7942555cf151aa6ff
Quang Tu Nguyen <quangtu2301@gmail.com>
2026-05-25 17:16:11 +1000

    Add: merge-intervals - submission-17

---
+++ Data Structures & Algorithms/merge-intervals/submission-17.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        const res = []
+
+        intervals.sort((a,b) => a[0] - b[0])
+
+        for (let i = 0; i < intervals.length; i++){
+            const [s2,e2] = intervals[i]
+            if (res.length >= 1){
+                const [s1,e1] = res[res.length-1]
+                if (s1 <= s2 && s2 <= e1){
+                    res[res.length - 1] = [Math.min(s1,s2), Math.max(e1,e2)]
+                    continue
+                }
+            }
+            res.push([s2,e2])
+        }
+
+        return res
+
+
+    }
+}
