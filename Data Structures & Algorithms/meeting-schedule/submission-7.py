9d5664c6e89769a71361469b8b216079daf143bd
Quang Tu Nguyen <quangtu2301@gmail.com>
2026-06-03 00:02:32 +1000

    Add: meeting-schedule - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule/submission-7.py
@@ -0,0 +1,16 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        sort = sorted(intervals, key= lambda x : x.start)
+        for i in range(1, len(sort)):
+            if sort[i].start < sort[i-1].end:
+                return False
+
+        return True
