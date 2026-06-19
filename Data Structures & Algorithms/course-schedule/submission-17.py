08574aaea0144d89813deed91a4fac8c43f9cce5
Quang Tu Nguyen <quangtu2301@gmail.com>
2026-06-19 16:42:53 +1000

    Add: course-schedule - submission-17

---
+++ Data Structures & Algorithms/course-schedule/submission-17.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preq = {}
+        for p in prerequisites:
+            if p[0] not in preq:
+                preq[p[0]] = []
+            preq[p[0]].append(p[1])
+
+        def dfs(c, visited):
+            if c not in preq:
+                return True
+            
+            if c in visited:
+
+                return False
+
+            visited.add(c)
+
+            for course in preq[c]:
+                if not dfs(course, visited):
+                    return False
+            
+            preq[c] = []
+            visited.remove(c)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i, set()):
+                return False
+
+        return True
+
+
+        
